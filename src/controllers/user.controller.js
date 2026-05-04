2e9dd84ac4b615cd0e291c557707361a4ddde2ce
Dipan <roydipan05@gmail.com>
2026-05-04 22:21:29 +0530

    refactor logoutUser

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -138,8 +138,8 @@ const logoutUser = asyncHandler(async (req, res) => {
   await User.findByIdAndUpdate(
     req.user._id,
     {
-      $set: {
-        refreshToken: undefined,
+      $unset: {
+        refreshToken: 1, // this removes the refreshToken field from the user's document in the database
       },
     },
     {
